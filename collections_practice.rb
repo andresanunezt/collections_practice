561731913a87587f99660b3cb44b2ca2fce8c2b5
Andrés Nuñez <andresanunez98@gmail.com>
2021-03-01 13:26:19 -0500

    commit tree ff09b8f7305b45c575352961a6a1f9e7aef9e117

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
 
+def sort_array_asc(array)
+array.sort
+end
+
+def sort_array_desc(array)
+    array.sort do |a,b| 
+        b <=> a
+    end
+end
+
+def sort_array_char_count(array)
+array.sort do|a,b| 
+    a.length <=> b.length
+end
+end
+
+def swap_elements(array)
+    array[0],array[1],array[2] = array[0],array[2],array[1]
+end
+
+def reverse_array(array)
+array.reverse
+end
+
+def kesha_maker(array)
+
+array.each {|word| word[2]= "$"}
+
+end
+
+def find_a(array)
+array.select {|word| word.start_with?("a")}
+end
+
+def sum_array(array)
+array.inject(0,:+) 
+end
+ 
+def add_s(array)
+    array.each_with_index.map do |word, index|
+      index == 1 ? word : word + "s"
+    end
+  end
